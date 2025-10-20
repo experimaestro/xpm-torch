734e4a627e78b8e879d99d58efea08624a22ef64
Victor MORAND <morand@isir.upmc.fr>
2025-10-20 11:51:32 +0200

    update xpmTorchModel

--- src/xpm_torch/xpmModel.py
+++ src/xpm_torch/xpmModel.py
@@ -5,7 +5,7 @@ from typing import (
     Union,
     Optional,
 )
-import torch, os, json, logging
+import torch, os, json, logging, shutil
 import torch.nn as nn
 from experimaestro.core.arguments import Meta
 from experimaestro import (
@@ -13,6 +13,7 @@ from experimaestro import (
     Task,
     LightweightTask,
     Param,
+    Constant,
     DataPath,
     serialize,
     deserialize,
@@ -27,7 +28,8 @@ T = TypeVar("T", bound="xpmTorchHubModule")
 
 class xpmTorchHubModule(nn.Module, Config, ModelHubMixin):
     """
-    Generic PyTorch module for experimaestro, compatible with Hugging Face Hub.
+    Generic PyTorch module for experimaestro, 
+    compatible with Hugging Face Hub via [ModelHubMixin](https://huggingface.co/docs/huggingface_hub/en/package_reference/mixins).
 
     Example usage:
     ```python
@@ -77,24 +79,6 @@ class xpmTorchHubModule(nn.Module, Config, ModelHubMixin):
     ```
     """
 
-    _config: dict
-    """Module *initial* configuration, can be used to recreate module"""
-
-    @property
-    def config(self):
-        return self._config
-
-    @config.setter
-    def config(self, config: dict):
-        if self._initialized:
-            raise AttributeError(
-                "config is immutable and cannot be modified after initialization"
-            )
-        self._config = config
-
-    def get_xpm_cfg(self):
-        return self.C(**self.config)
-
     class Loader(LightweightTask):
         """Loader for the xpmTorchHubModule"""
 
@@ -103,18 +87,44 @@ class xpmTorchHubModule(nn.Module, Config, ModelHubMixin):
 
         def execute(self):
             """Loads the model from disk using the given serialization path"""
-            state_dict = torch.load(self.parameters)
+            state_dict = torch.load(self.parameters, map_location="cpu")
             self.model.load_state_dict(state_dict)
 
     def __post_init__(self):
+        """Initialize the module. Child classes should override this method to initialize their layers and other stuff."""
         super().__post_init__()
-        self._initialized = False
-        self.config = {name: getattr(self, name) for name in self.__xpmtype__.arguments}
-        self._initialized = True
+        
 
     @property
     def device(self):
         return next(self.parameters()).device
+
+    def count_parameters(self):
+        """Count the number of parameters in the model"""
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+    # def save_pretrained(
+    #     self,
+    #     save_directory,
+    #     *,
+    #     config=None,
+    #     repo_id=None,
+    #     push_to_hub=False,
+    #     model_card_kwargs=None,
+    #     **push_to_hub_kwargs,
+    # ):
+    #     """ Save the model weights and config to the specified directory.
+    #     see <https://huggingface.co/docs/huggingface_hub/en/package_reference/mixins#huggingface_hub.ModelHubMixin>
+    #     """
+
+    #     return super().save_pretrained(
+    #         save_directory,
+    #         config=config,
+    #         repo_id=repo_id,
+    #         push_to_hub=push_to_hub,
+    #         model_card_kwargs=model_card_kwargs,
+    #         **push_to_hub_kwargs,
+    #     )
 
     def _save_pretrained(self, save_directory: Path):
         """
@@ -124,18 +134,19 @@ class xpmTorchHubModule(nn.Module, Config, ModelHubMixin):
         # Save model weights to directory
         params_path = Path("parameters.pth")
         torch.save(self.state_dict(), params_path)
-        xpm_cfg = self.get_xpm_cfg()
-
+        if save_directory.exists():
+            logging.warning(f"Folder {save_directory} already exists, overwriting it")
+            shutil.rmtree(save_directory)
         # serialize whole model and dependancies
+        xpm_config = self.__config__
         serialize(
-            xpm_cfg,
+            xpm_config,
             save_directory,
-            init_tasks=[self.Loader.C(model=xpm_cfg, parameters=params_path)],
+            init_tasks=[self.Loader.C(model=xpm_config, parameters=params_path)],
         )
 
-        # Save initial config (will likely have issues serializing parent models)
-        with open(save_directory / "config.json", "w") as f:
-            json.dump(self.config, f)
+        # # finally delete temporary files
+        # params_path.unlink(missing_ok=True)
 
     @classmethod
     def _from_pretrained(
@@ -157,9 +168,15 @@ class xpmTorchHubModule(nn.Module, Config, ModelHubMixin):
         see <https://huggingface.co/docs/huggingface_hub/en/package_reference/mixins#huggingface_hub.ModelHubMixin>
         """
         if os.path.isdir(model_id):
+            #model is save Locally
             save_directory = Path(model_id)
-
-            return deserialize(save_directory, as_instance=True)
+            model_cfg, [loader] = deserialize(save_directory, as_instance=False)
+            #get Loader Task
+            loader = loader.instance()
+            loader.execute()
+            model = loader.model
+            object.__setattr__(model, "__config__", model_cfg)
+            return model
 
         else:
 
@@ -208,48 +225,53 @@ class xpmTorchHubModule(nn.Module, Config, ModelHubMixin):
         return cfg.instance()
 
 
-# Example child module
-class MyTorchxpmTorchHubModule(
-    xpmTorchHubModule,
-    library_name="my-org/my-model",
-    tags=["example", "torch"],
-    repo_url="https://huggingface.co/my-org/my-model",
-    paper_url="https://arxiv.org/abs/???",
-):
-    """Example xpmTorchHubModule implementation, now all saving and loading is taken care of under the hood"""
-
-    ## Child parameters
-    input_dim: Param[int] = 100
-    """Input dimension"""
-
-    hidden_dim: Param[int] = 200
-    """Hidden dimension"""
-
-    output_dim: Param[int] = 10
-    """Output dimension"""
-
-    def __post_init__(self):
-        super().__post_init__()
-        self.fc1 = nn.Linear(self.input_dim, self.hidden_dim)
-        self.fc2 = nn.Linear(self.hidden_dim, self.output_dim)
-        logging.debug("Initialized layers: %s, %s", self.fc1, self.fc2)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-
-
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
 
-    model_id = "test-model"
-    save_path = Path("./model")
 
-    create_new_model = True
+    # Example child module
+    class MyTorchxpmTorchHubModule(
+        xpmTorchHubModule,
+        library_name="my-org/my-model",
+        tags=["torch", "experimaestro"],
+        repo_url="https://github.com/VictorMorand/test-model",
+        paper_url="https://arxiv.org/abs/???",
+    ):
+        """Example xpmTorchHubModule implementation, now all saving and loading is taken care of under the hood"""
+
+        ## Child parameters
+        input_dim: Param[int] = 100
+        """Input dimension"""
+
+        hidden_dim: Param[int] = 200
+        """Hidden dimension"""
+
+        output_dim: Param[int] = 10
+        """Output dimension"""
+
+        version: Constant[str] = "1.0"
+
+        def __post_init__(self):
+            super().__post_init__()
+            # self._config = self.__config__
+
+            self.fc1 = nn.Linear(self.input_dim, self.hidden_dim)
+            self.fc2 = nn.Linear(self.hidden_dim, self.output_dim)
+            logging.debug("Initialized layers: %s, %s", self.fc1, self.fc2)
+
+        def forward(self, x: torch.Tensor) -> torch.Tensor:
+            x = torch.relu(self.fc1(x))
+            x = self.fc2(x)
+            return x
+
+
+    model_id = "VictorMorand/test-model"
+    save_path = Path("./test_model")
+
     create_new_model = False
+    create_new_model = True
 
     if create_new_model:
         # create configuration
@@ -259,7 +281,9 @@ if __name__ == "__main__":
 
         model: MyTorchxpmTorchHubModule = cfg.instance()
         print(model)
-
+        xpm_config = model.__config__
+        print(type(xpm_config),xpm_config)
+        
         # test it
         input = torch.randn(1, 50)
         output = model(input)
@@ -267,14 +291,27 @@ if __name__ == "__main__":
         print(output)
 
         model.save_pretrained(save_path)
-        model.push_to_hub(model_id)
+        # model.push_to_hub(model_id)
 
-    # save_path = Path("./test_model")
-    # try to load from Hub
+        # try to reload it
+        logging.info("Reloading model from %s", save_path)
+        newModel = MyTorchxpmTorchHubModule.from_pretrained(save_path)
 
-    newModel = MyTorchxpmTorchHubModule.from_pretrained(
-        model_id, force_download=True, local_files_only=False
-    )
+    else:
+        # save_path = Path("./test_model")
+        # try to load from Hub
+
+        newModel = MyTorchxpmTorchHubModule.from_pretrained(
+            model_id, force_download=True, local_files_only=False
+        )
 
     logging.info("New model: %s", newModel)
-    logging.info("New model config: %s", newModel.config)
+    logging.info("New model config: %s", newModel.__config__)
+
+    # # # Model that can be re-used in experiments
+    # # model, init_tasks = AutoModel.load_from_hf_hub("xpmir/SPLADE_DistilMSE")
+
+    # # init_tasks[0].execute()
+    # # Use this if you want to actually use the model
+    # model = AutoModel.load_from_hf_hub("xpmir/SPLADE_DistilMSE", as_instance=True)
+    # print(model.rsv("walgreens store sales average", "The average Walgreens salary ranges..."))
